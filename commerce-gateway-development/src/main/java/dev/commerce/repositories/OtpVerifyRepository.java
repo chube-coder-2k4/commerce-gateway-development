69d23f728bd4a12007a6fab8e253d8ec3a44ae84
chube-coder-2k4 <qhuyddbmt123@gmail.com>
2025-11-14 22:41:19 +0700

    repository for otp and user

--- commerce-gateway-development/src/main/java/dev/commerce/repositories/OtpVerifyRepository.java
+++ commerce-gateway-development/src/main/java/dev/commerce/repositories/OtpVerifyRepository.java
@@ -2,8 +2,9 @@ package dev.commerce.repositories;
 
 import dev.commerce.entitys.OtpVerify;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface OtpVerifyRepository extends JpaRepository<OtpVerify, UUID> {
+public interface OtpVerifyRepository extends JpaRepository<OtpVerify, UUID>, JpaSpecificationExecutor<OtpVerify> {
 }
--- commerce-gateway-development/src/main/java/dev/commerce/repositories/UserRepository.java
+++ commerce-gateway-development/src/main/java/dev/commerce/repositories/UserRepository.java
@@ -2,8 +2,9 @@ package dev.commerce.repositories;
 
 import dev.commerce.entitys.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<Users, UUID> {
+public interface UserRepository extends JpaRepository<Users, UUID>, JpaSpecificationExecutor<Users>{
 }
