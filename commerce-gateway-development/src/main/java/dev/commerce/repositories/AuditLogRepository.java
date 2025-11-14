19d99591325c4203d58c98611b5766987af44d4c
chube-coder-2k4 <qhuyddbmt123@gmail.com>
2025-11-14 22:43:33 +0700

    Auditlog tracking some operator

---
+++ commerce-gateway-development/src/main/java/dev/commerce/repositories/AuditLogRepository.java
@@ -0,0 +1,12 @@
+package dev.commerce.repositories;
+
+import dev.commerce.entitys.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AuditLogRepository extends JpaRepository<AuditLog, UUID>, JpaSpecificationExecutor<AuditLog> {
+}
